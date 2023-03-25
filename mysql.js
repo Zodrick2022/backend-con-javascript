94db5a692994408dfa73a45dd91f7bf4071cc4a2
Eduard Paredes <106275524+Zodrick2022@users.noreply.github.com>
2023-03-25 13:49:07 -0500

    Add files via upload

---
+++ mysql.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql')
+//vamos a nuestro servidor y clickeamos cuentas de usuarios y damos clic en agregar cuenta de usuario
+//le damos un nombre de usuario, el nombre del servidor que sea 'localhost' y una contraseña y le damos permisos a la base de datos
+// y listo ya tenemos una base de datos con un usuario y contraseña conectada con javascript
+const connection = mysql.createConnection({
+    localhost: 'localhost',
+    user: 'manolo',
+    password: 'manolo',
+    database: 'usuarios'
+})
+
+connection.connect((err) => {
+    if(err) throw err
+    console.log('La conexión a la base de datos se ha realizado correctamente')
+})
+//conexion de la base de datos con javascript
+// 1) npm init -y
+// 2)  node install mysql
+// 3) npm install mysql 
+// 4) node .\mysql.js   
+
+connection.query('SELECT * FROM usuarios', (err, result) => {
+    if(err) throw err
+    console.log('Los datos de la tabla son: ', result) //result muestra los datos de la tabla sin esto no parece nada
+    console.log('La cantidad de resultados es:' , result.length)//result.length muestra la cantidad de resultados
+    const usuarioUno = result[0] //para motrar el priner usuario siempre se pone un 0 ya que este lo cuenta como numero de inicio
+    console.log('El primer usuario es: ', usuarioUno)
+    const usuarioDos = result[1]
+    console.log('El segundo usuario es: ', usuarioDos)
+})
---
+++ usuarios.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2023 a las 19:30:21
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `usuarios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `Nombre` varchar(11) NOT NULL,
+  `Apellido` varchar(11) NOT NULL,
+  `Edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `Nombre`, `Apellido`, `Edad`) VALUES
+(1, 'Daniel', 'Garcia', 17),
+(2, 'Cecilia', 'Gomez', 34);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
